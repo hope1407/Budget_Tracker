a7a0aa7e264e2c5e53e84fee8fa684f1495b6415
Hope <johnhope354@gmail.com>
2021-09-03 15:54:10 -0500

    updates to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,4 +10,51 @@ const FILES_TO_CACHE = [
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
 
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(PRECACHE).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
 
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== PRECACHE && key !== RUNTIME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(RUNTIME).then((cache) => {
+        return fetch(event.request).then((response) => {
+          if (response.status === 200) {
+            cache.put(event.request.url, response.clone());
+          }
+          return response;
+        })
+        .catch((err) => {
+          return cache.match(event.request);
+        })
+      })
+    );
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
