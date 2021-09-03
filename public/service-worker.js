65f69bac1eebe7528a04c1d48ddbca692327264a
Hope <johnhope354@gmail.com>
2021-09-03 15:12:58 -0500

    service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,13 @@
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/index.js',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-  ];
-  
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+
