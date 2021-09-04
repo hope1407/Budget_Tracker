a6972f2fe1e2b109544bc74b86fe044651f7b0a3
Hope <johnhope354@gmail.com>
2021-09-04 13:58:20 -0500

    almost working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,15 @@
 const FILES_TO_CACHE = [
   "/",
+  "/db.js",
   "/index.html",
   "/assets/js/index.js",
   "/assets/css/styles.css",
   "/assets/icons/icon-192x192.png",
-  "/icons/icon-512x512.png",
+  "/assets/icons/icon-512x512.png",
 ];
 
 const PRECACHE = "precache-v1";
 const RUNTIME = "runtime";
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      function (registration) {
-        // Registration was successful
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      function (err) {
-        // registration failed :(
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
 
 self.addEventListener("install", function (event) {
   event.waitUntil(
@@ -47,22 +30,40 @@ self.addEventListener("activate", function (event) {
   self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-  if (event.request.url.startsWith(self.location.origin)) {
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
     event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        if (cachedResponse) {
-          return cachedResponse;
-        }
+      caches.open(FILES_TO_CACHE).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
 
-        return caches.open(RUNTIME).then((cache) => {
-          return fetch(event.request).then((response) => {
-            return cache.put(event.request, response.clone()).then(() => {
-              return response;
-            });
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
           });
-        });
-      })
+      }).catch(err => console.log(err))
     );
+
+    return;
   }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
 });
