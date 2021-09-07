7b50c1c9d17f458a5887745df2c7b859df5ad1fa
Hope <johnhope354@gmail.com>
2021-09-07 18:19:12 -0500

    troubleshooting heroku

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/dbBudget", {
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/budget", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
